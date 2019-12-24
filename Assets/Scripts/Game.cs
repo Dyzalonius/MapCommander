2d37b089bcfbc1ac58d4ebae5dedda3778068a39
dyzalonius <dyzalonius@gmail.com>
2019-12-24 15:20:20 +0100

    Change terrain so it creates a mesh of the ground instead of spawning a plane object, also moving the unity project into the main folder

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField]
+    public Player Player;
+
+    [SerializeField]
+    private GameObject unitPrefab;
+
+    [SerializeField]
+    private Terrain terrain;
+
+    [HideInInspector]
+    public List<Unit> Units = new List<Unit>();
+
+    [HideInInspector]
+    public static Game Instance { get; private set; } // static singleton
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.N))
+        {
+            Unit newUnit = Instantiate(unitPrefab, transform).GetComponent<Unit>();
+            Units.Add(newUnit);
+        }
+    }
+}
