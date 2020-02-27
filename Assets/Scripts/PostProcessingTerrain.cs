ac079d41762ed69d66cb1dc2864e697de63c5cf2
dyzalonius <dyzalonius@gmail.com>
2020-02-27 17:19:50 +0100

    Change terrain post processing effect shader to actually work for the most part

--- Assets/Scripts/PostProcessingTerrain.cs
+++ Assets/Scripts/PostProcessingTerrain.cs
@@ -7,37 +7,11 @@ using UnityEngine;
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class PostProcessingTerrain : MonoBehaviour
 {
-	[Range(0, 16)]
-	public int iterations = 0;
+	[SerializeField]
+	private Material material;
 
 	private void OnRenderImage(RenderTexture source, RenderTexture destination)
 	{
-		//apply our material to the ouput
-		int width = source.width;
-		int height = source.height;
-		RenderTextureFormat format = source.format;
-
-		RenderTexture currentDestination = RenderTexture.GetTemporary(width, height, 0, format);
-		Graphics.Blit(source, currentDestination);
-		RenderTexture currentSource = currentDestination;
-
-		for (int i = 0; i < iterations; i++)
-		{
-			//sample half the resolution every iteration
-			width /= 2;
-			height /= 2;
-
-			if (height < 2)
-			{
-				break;
-			}
-
-			currentDestination = RenderTexture.GetTemporary(width, height, 0, format);
-			Graphics.Blit(currentSource, currentDestination);
-			RenderTexture.ReleaseTemporary(currentSource);
-			currentSource = currentDestination;
-		}
-
-		Graphics.Blit(currentSource, destination);
+		Graphics.Blit(source, destination, material);
 	}
 }
